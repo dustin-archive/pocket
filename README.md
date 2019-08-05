26f90214c717ce306d21daa43eac13d33e90cb1c
Dustin <7419947+whaaaley@users.noreply.github.com>
2019-08-05 05:27:26 -0500

    1.0.0 (#2)
    * commit project
    
    * defer rerenders
    
    * defer renders & refactor
    
    * fix clone & add curried actions & fix requestAnimationFrame
    
    * Create .gitignore
    
    * brower tests
    
    * fix actions in actions
    
    * remove const and let
    
    * remove arrow function
    
    * Update package.json
    
    * expand conditional statements & remove clone function
    
    * fix actions in actions without let & simplify structure
    
    * rename opts to props
    
    * skip render if state is unchanged
    
    * Update README.md
    
    * Update LICENSE
    
    * Update README.md
    
    * Update package.json
    
    * Update actions.js

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
+
 # pocket
+
+> A simple state manager.
+
+## Install
+
+```
+npm github:whaaaley/pocket
+```
+
+## Use
+
+Use alongside your favorite view library.
+
+```js
+import { patch } from 'superfine'
+import { a, div } from 'html'
+import pocket from 'pocket'
+
+const view = (state, actions) => {
+  return div([
+    a({ onclick: () => actions.decrement() }, '-'),
+    div(, state.count),
+    a({ onclick: () => actions.increment() }, '+')
+  ])
+}
+
+let node
+
+pocket({
+  state: {
+    count: 0
+  },
+  actions: {
+    decrement: () => state => ({ count: state.count - 1 }),
+    increment: () => state => ({ count: state.count + 1 })
+  },
+  render: (state, actions) => {
+    node = patch(node, view(state, actions), document.body)
+  }
+})
+```
---
+++ index.js
@@ -0,0 +1,47 @@
+
+function pocket (props) {
+  var assign = Object.assign
+  var state = assign({}, props.state)
+  var actions = assign({}, props.actions)
+  var lock = false
+
+  for (var key in actions) {
+    wire()
+  }
+
+  render()
+
+  return actions
+
+  function wire () {
+    var action = actions[key]
+
+    actions[key] = function (data) {
+      var result = action(data)
+
+      if (typeof result === 'function') {
+        result = result(state, actions)
+      }
+
+      if (result && result !== state) {
+        state = assign({}, state, result)
+
+        if (!lock) {
+          render(lock = true)
+        }
+      }
+
+      return result
+    }
+  }
+
+  function render () {
+    lock = false
+
+    requestAnimationFrame(function () {
+      props.render(state, actions)
+    })
+  }
+}
+
+export default pocket

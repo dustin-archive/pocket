5fd7f32551e0562f4de7af3c3efcabdc78d43c2f
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2019-08-05 04:45:32 -0500

    Update actions.js

--- test/actions.js
+++ test/actions.js
@@ -11,101 +11,116 @@ const equal = (value, equals, message) => {
   console.log('  ...')
 }
 
+// - - - - - - - -
+// # Test 1
+// - - - - - - - -
+
 const test1 = () => {
   let i = 0
 
-  const main = pocket({
+  const dispatch = pocket({
     state: {
       value: 1
     },
-    actions: {
-      up: () => state => ({ value: state.value + 1 })
-    },
     render: state => {
-      if (i === 0) equal(state.value, 1, 'value should be 1')
-      if (i === 1) equal(state.value, 2, 'value should be 2')
+      if (i === 0) equal(state.value, 1, 'Test 1: value should be 1')
+      if (i === 1) equal(state.value, 2, 'Test 1: value should be 2')
       i++
     }
   })
 
-  setTimeout(() => main.up(), 1000)
+  const up = state => ({ value: state.value + 1 })
+
+  setTimeout(() => dispatch([ up ]), 1000)
 }
 
 test1()
 
+// - - - - - - - -
+// # Test 2
+// - - - - - - - -
+
 const test2 = () => {
   let i = 0
 
-  const main = pocket({
+  const dispatch = pocket({
     state: {},
-    actions: {
-      update: data => data
-    },
     render: state => {
-      if (i === 0) equal(state.foo, void 0, 'foo should be undefined')
-      if (i === 1) equal(state.foo, 'bar', 'foo should be bar')
+      if (i === 0) equal(state.foo, void 0, 'Test 2A: foo should be undefined')
+      if (i === 1) equal(state.foo, 'bar', 'Test 2B: foo should be bar')
       i++
     }
   })
 
-  setTimeout(() => main.update({ foo: 'bar' }), 1000)
+  const update = data => () => data
+
+  setTimeout(() => dispatch([ update({ foo: 'bar' }) ]), 1000)
 }
 
 test2()
 
+// - - - - - - - -
+// # Test 3
+// - - - - - - - -
+
 const test3 = () => {
   let i = 0
 
-  const main = pocket({
+  const dispatch = pocket({
     state: {
       value: 2
     },
-    actions: {
-      up: () => state => ({ value: state.value + 1 }),
-      delay: () => (s, actions) => {
-        setTimeout(() => actions.up(), 1000)
-      }
-    },
     render: state => {
-      if (i === 0) equal(state.value, 2, 'value should be 2')
-      if (i === 1) equal(state.value, 3, 'value should be 3')
+      if (i === 0) equal(state.value, 2, 'Test 3A: value should be 2')
+      if (i === 1) equal(state.value, 2, 'Test 3B: value should be 2')
+      if (i === 2) equal(state.value, 3, 'Test 3C: value should be 3')
       i++
     }
   })
 
-  setTimeout(() => main.delay(), 1000)
+  const up = state => {
+    return { value: state.value + 1 }
+  }
+  const delay = (state) => {
+    setTimeout(() => dispatch([ up ]), 1000)
+  }
+
+  setTimeout(() => dispatch([ delay ]), 1000)
 }
 
 test3()
 
+// - - - - - - - -
+// # Test 4
+// - - - - - - - -
+
 const test4 = () => {
   let i = 0
 
-  const main = pocket({
+  const dispatch = pocket({
     state: {
       value: 3
     },
-    actions: {
-      up: () => state => ({ value: state.value + 1 }),
-      foo: () => (s, actions) => {
-        actions.up()
-        return { foo: true }
-      }
-    },
     render: state => {
       if (i === 0) {
-        equal(state.value, 3, 'value should be 3')
-        equal(state.foo, void 0, 'foo should be undefined')
+        equal(state.value, 3, 'Test 4A: value should be 3')
+        equal(state.foo, void 0, 'Test 4B: foo should be undefined')
       }
       if (i === 1) {
-        equal(state.value, 4, 'value should be 4')
-        equal(state.foo, true, 'value should be true')
+        equal(state.value, 4, 'Test 4C: value should be 4')
+        equal(state.foo, true, 'Test 4D: value should be true')
       }
       i++
     }
   })
 
-  setTimeout(() => main.foo(), 1000)
+  const up = state => ({ value: state.value + 1 })
+  const foo = () => {
+    dispatch([ up ])
+    return { foo: true }
+  }
+
+  setTimeout(() => dispatch([ foo ]), 1000)
 }
 
 test4()
